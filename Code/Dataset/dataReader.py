938238c97c93a1648ecd6bbd37b939b35660cd57
unknown <denizsungurtekin@gmail.com>
2022-02-02 12:40:45 +0100

    remove data

--- Code/Dataset/dataReader.py
+++
@@ -1,9 +0,0 @@
-from numpy import genfromtxt
-
-def csv(filename):
-    return genfromtxt(filename, delimiter=',',dtype=None,encoding=None)
-
-# from Dataset import dataReader as reader
-#
-# # x = reader.csv("../../Dataset/annotated Cytometrie/Labeled_Gated F15-631.csv") example use of reader
-# # print(x)
