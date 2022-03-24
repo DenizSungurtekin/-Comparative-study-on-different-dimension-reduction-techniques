e596b19a26ef60de1c343f2929eea3c2ce90a9db
unknown <denizsungurtekin@gmail.com>
2022-03-24 12:09:51 +0100

    Fix tsne perplexity binary search, add umap k binary search and getting a,b parameter from least square method, umap abd tsne complete with basic intialization method of the embedding

--- Code/Implementations/Tsne/tsne.py
+++ Code/Implementations/Tsne/tsne.py
@@ -1,4 +1,32 @@
+from sklearn.metrics.pairwise import euclidean_distances
 import numpy as np
+
+def compute_proba_p_row(distance,sigma,index):
+    probas = np.exp(-(distance)/(2*(np.power(sigma,2))))
+    probas[index] = 1e-6  # not zero to avoid instability when computing entropy
+    return probas/probas.sum()
+
+def compute_perplexity(proba_row):
+    entropy = -np.sum(proba_row*np.log2(proba_row))
+    return np.power(2,entropy)
+
+def find_optimal_sigma(target_perplexity,distance_row,row_index,low = 1,upper = 100,tol = 10e-5,max_ite = 50): #Binary search
+    for i in range(max_ite):
+        sigma = (low + upper) / 2.
+        perplexity_current = compute_perplexity(compute_proba_p_row(distance_row, sigma, row_index))
+        if np.abs(perplexity_current-target_perplexity) < tol:
+            break
+        else:
+            if perplexity_current < target_perplexity:
+                low = sigma
+            else:
+                upper = sigma
+    return sigma
+
+def compute_prob_p(distance,sigmas):
+    N = distance.shape[0]
+    matrix = np.concatenate([[compute_proba_p_row(distance[i],sigmas[i],i) for i in range(N)]],axis = 0)
+    return matrix
 
 def negatif_squared_distance(X):
     # Compute matrix of negative squared euclidean
@@ -7,88 +35,36 @@ def negatif_squared_distance(X):
     distance_sqr = np.add(np.add(-2 * np.dot(X, X.T), somme).T, somme)
     return -distance_sqr
 
-def compute_p_distribution(X, diag_zero=True):
-    p_cond_ji = np.exp(X)
-    if diag_zero:
-        np.fill_diagonal(p_cond_ji, 0.) #pi|i = 0
-
-    p_cond_ji = p_cond_ji + 1e-7  # To be able to compute entropie ( log_2(pi|j) if = 0 => problem )
-    return p_cond_ji / p_cond_ji.sum(axis=1).reshape([-1, 1]) # Normalize each row
-
-
-def compute_prob_matrix(distances, sigmas):
-    return compute_p_distribution(distances / 2. * np.square(sigmas.reshape((-1, 1))))
-
-def binary_search(compute_perplexity, target_perplexity, row,rows,max_iter=3000,tolerance=1e-5, upper=500.,lower=1e-15):
-    for i in range(max_iter):
-        sigma = (lower + upper) / 2.
-        perplexity = compute_perplexity(sigma)
-        if perplexity > target_perplexity:
-            upper = sigma
-        else:
-            lower = sigma
-        if np.abs(perplexity - target_perplexity) <= tolerance:
-            break
-        if i == max_iter - 1:
-            rows.append(row) #To know for which row we do not have an ideal perplexity
-
-    return sigma,rows
-
-def calc_perplexity(prob_matrix):
-    # Compute perplexity of each row
-    entropy = -np.sum(prob_matrix * np.log2(prob_matrix), 1) #Carefull when proba = 0
-    perplexity = 2 ** entropy
-    return perplexity
-
-
-def perplexity(distances, sigmas):
-    #From distances and sigmas to perplexity vector (for each row)
-    return calc_perplexity(compute_prob_matrix(distances, sigmas))
-
-
-def find_optimal_sigmas(distances, target_perplexity):
-    # We want the sigmas for each row giving the target perplexity
-    sigmas = []
-    rows = []
-    for i in range(distances.shape[0]):
-        compute_perplexity = lambda sigma: perplexity(distances[i:i+1, :], np.array(sigma))
-        optimal_sigma,rows = binary_search(compute_perplexity, target_perplexity,i,rows)
-        sigmas.append(optimal_sigma)
-
-    print("Warning, an optimal standard deviation can't be found for rows: ",rows)
-    return np.array(sigmas)
+def compute_q(Y):
+    q = np.power(1. - negatif_squared_distance(Y), -1)
+    np.fill_diagonal(q, 0.)  # qi,i
+    return q / np.sum(q), q  # normalize in the entire matrix and return also the inverse distance
 
 def p_conditional_to_joint(P):
     return (P + P.T) / (2. * P.shape[0])
 
-def compute_joint_proba(data, target_perplexity): # Compute joint probability from data and target perplexity
-    distances = negatif_squared_distance(data)
-    sigmas = find_optimal_sigmas(distances, target_perplexity)
-    p_conditional = compute_prob_matrix(distances, sigmas)
+def compute_joint_proba(data, target_perplexity):  # Compute joint probability from data and target perplexity
+    N = data.shape[0]
+    distances = np.square(euclidean_distances(data,data))
+    sigmas = np.asarray([find_optimal_sigma(target_perplexity,distances[i],i) for i in range(N)])
+    p_conditional = compute_prob_p(distances, sigmas)
     P = p_conditional_to_joint(p_conditional)
     return P
 
-def compute_q(Y):
-    q = np.power(1. - negatif_squared_distance(Y), -1)
-    np.fill_diagonal(q, 0.) # qi,i
-    return q / np.sum(q), q # normalize in the entire matrix and return also the inverse distance
-
 def tsne_grad(P, Q, Y, inv_distances):
-    pq_sub_exp = np.expand_dims(P - Q, 2) #(pij −qij)
-    y_sub = np.expand_dims(Y, 1) - np.expand_dims(Y, 0) # (yi −y)
-    dist_exp = np.expand_dims(inv_distances, 2)  #(1+||yi−yj||2)**-1
+    pq_sub_exp = np.expand_dims(P - Q, 2)  # (pij −qij)
+    y_sub = np.expand_dims(Y, 1) - np.expand_dims(Y, 0)  # (yi −y)
+    dist_exp = np.expand_dims(inv_distances, 2)  # (1+||yi−yj||2)**-1
     y_diffs_wt = y_sub * dist_exp
     # We sum over the j c.f formula gradient
-    grad = 4. * (pq_sub_exp * y_diffs_wt).sum(1) #(pij−qij)(yi−yj)(1+||yi−yj||2)−1
+    grad = 4. * (pq_sub_exp * y_diffs_wt).sum(1)  # (pij−qij)(yi−yj)(1+||yi−yj||2)−1
     return grad
 
 
-def tsne_reduction(data, target_dim, target_perplexity, iterations, lr = 0.01, momentum = False):
-
-    # There is a lot of different initialisation method: The usual initialization routine for t-SNE is to start from a small random gaussian distribution,
+def tsne_reduction(data, target_dim, target_perplexity, iterations = 200, lr=0.01, momentum=False):
+    # There is a lot of different initialisation method: The usual initialization routine for t-SNE is to start from a small random gaussian distribution, TODO
     # and use “early exaggeration” for the first 100 or so iterations where the input probabilities are multiplied by 4
-
-    Y = np.random.normal(0,1,size=(data.shape[0],target_dim)) * 4
+    Y = np.random.normal(0, 1, size=(data.shape[0], target_dim)) * 4
     P = compute_joint_proba(data, target_perplexity)
 
     if momentum:
@@ -97,10 +73,10 @@ def tsne_reduction(data, target_dim, target_perplexity, iterations, lr = 0.01, m
 
     for i in range(iterations):
 
-        Q, inverser_distances = compute_q(Y)
-        grads = tsne_grad(P,Q,Y,inverser_distances)
+        Q, inverse_distances = compute_q(Y)
+        grads = tsne_grad(P, Q, Y, inverse_distances)
 
-        Y = Y - lr*grads
+        Y = Y - lr * grads
         if momentum:
             Y += momentum * (Y_m1 - Y_m2)
             Y_m2 = Y_m1.copy()
@@ -108,12 +84,9 @@ def tsne_reduction(data, target_dim, target_perplexity, iterations, lr = 0.01, m
 
     return Y
 
-N = 40
-M = 20
-data = np.random.rand(N,M)
-print("Orginal data: ",data)
-print("")
-target_perplexity = 5
-Y = tsne_reduction(data,2,5,100)
-print("")
-print("Embeed data: ",Y)
+N = 10
+M = 5
+PERPLEXITY = 5
+TARGET_DIMENSION = 2
+data = np.random.normal(0, 1, size=(N, M)) * 4
+print(tsne_reduction(data,TARGET_DIMENSION,PERPLEXITY))
--- Code/Implementations/Umap/umap.py
+++ Code/Implementations/Umap/umap.py
@@ -1,26 +1,8 @@
 from sklearn.metrics.pairwise import euclidean_distances
 import numpy as np
-
-N = 100
-M = 20
-
-a = 1
-b = 1 # UMAP finds a and b from non-linear least-square fitting to the piecewise function with the min_dist hyperparameter:
-
-min_dist = 0.25 # Hyperparametre influant sur a et b mais ici initialise à 1
-embed_dim = 2 #hyperparam
-#n_epoch -> hyperparam
-#n -> hyperparam , the number of neighbors to consider when approximating the local metric; utilise dans spectral embedding
-
-X_train = np.random.rand(N,M)
-dist = np.square(euclidean_distances(X_train, X_train)) #Matrice de distance au carré avec norme 2 -> euclidian
-rhos = [sorted(dist[i])[1] for i in range(N)] # Distance avec le voisin le plus proche de chaque point p_i
-
-#Calcul de l'entropie comme la somme des probas de chaque cellule de la matrice de distance pour choisir k = 2**Entropy pour definri sigmnas TODO
-sigmas = np.random.rand(N)
-
-## NORMALEMENT Y INITIALISE AVEC SPECTRAL EMBEDING A VOIR PLUS EN DETAIL
-Y = np.random.rand(N,embed_dim)
+import scipy.optimize as op
+import matplotlib.pyplot as plt
+import sklearn.manifold
 
 def computeProbMatrix(sigmas,dist,rhos): #Compute pi|j, sigmas 1d vector containing sgd for each point
     N = len(dist)
@@ -28,23 +10,23 @@ def computeProbMatrix(sigmas,dist,rhos): #Compute pi|j, sigmas 1d vector contain
     return prob_matrix
 
 def computeProba(dist,sigma,rho):
-    return np.exp(-((dist-rho)/sigma)) #Symetrique dans notre cas pas besoin de faire pij = pi|j + pj|i - pi|j*pj|i ATTENTION FORMULE A MODULE
+    return np.exp(-((dist-rho)/sigma))
 
+def p_conditional_to_joint(P):
+    return (P + P.T) - np.multiply(P,P.T)
 
-def computeProbEmbedMatrix(Y):
-    prob_matrix = np.power(1 + a * np.square(euclidean_distances(Y, Y))**b, -1)
-    # euclidean_distances(Y,Y) ex: ligne 1 contient distance de y_1 aux autre y -> dist[1][2] -> distance y1-y2
+def compute_k(P_row): #return k for a row
+    return 2**P_row.sum()
+
+def computeProbEmbedMatrix(Y,a,b):
+    prob_matrix = np.power(1 + a * np.square(euclidean_distances(Y, Y))**(2*b), -1)
     return prob_matrix
 
-# Optimization
 
-# def cross_entropy(ProbMatrix,EmbedProbMatrix): # +0.01 pour eviter 0 -> On s'interesse plus au gradient pour mettre a jour Y
-#     return - ProbMatrix * np.log(EmbedProbMatrix + 0.01) - (1 - ProbMatrix) * np.log(1 - EmbedProbMatrix + 0.01)
-
-def computeGrad(ProbaMatrix,Y): #Derivative of cross entropy
+def computeGrad(ProbaMatrix,Y,a,b): #Derivative cross entropy
 
     y_differences = np.expand_dims(Y, 1) - np.expand_dims(Y, 0)
-    embedProbMatrix = computeProbEmbedMatrix(Y)
+    embedProbMatrix = computeProbEmbedMatrix(Y,a,b)
     Q = np.dot(1 - ProbaMatrix, np.power(0.001 + np.square(euclidean_distances(Y, Y)), -1)) # Q contient
     np.fill_diagonal(Q, 0)
     Q = Q / np.sum(Q, axis = 1, keepdims = True) # Normalization pas dans u_map mais peut donner meilleur resultat
@@ -52,14 +34,77 @@ def computeGrad(ProbaMatrix,Y): #Derivative of cross entropy
     return 2 * b * np.sum(fact * y_differences * np.expand_dims(embedProbMatrix, 2), axis = 1)
 
 
-def main(X_train,min_dist,embed_dim,a=1,b=1,sigmas=sigmas,epochs = 100,learning_rate = 0.01):
-    dist = np.square(euclidean_distances(X_train, X_train))  # Matrice de distance au carré avec norme 2 -> euclidian
+def compute_p_row(dist,sigma,rho,normalization):
+    if normalization:
+        probas = np.asarray([np.exp(-((dist[i]-rho)/sigma)) for i in range(len(dist))])
+        somme = probas.sum()
+        probas = probas/somme
+        return probas
+    else:
+        return np.asarray([np.exp(-((dist[i]-rho)/sigma)) for i in range(len(dist))])
+
+def find_optimal_sigma(target_k,rho,dist_row,normalization,max_iterations = 30,tolerance = 1e-5,upper = 500.,low = 0.): #Binary search to find sigma with target k (nb of nearest neighbor)
+
+    for i in range(max_iterations): # if k big then sigma larger
+        current_sigma = (upper+low)/2
+        p_row = compute_p_row(dist_row,current_sigma,rho,normalization)
+        p_row = p_conditional_to_joint(p_row)
+        current_k = compute_k(p_row)
+        if np.abs(current_k-target_k) < tolerance:
+            break
+        else:
+            if current_k<target_k:
+                low = current_sigma
+            else:
+                upper = current_sigma
+
+    return current_sigma
+
+def find_best_a_b(min_dist): # On veut trouver a et b utilise dans le calcul de q. Idée est qu on a (1+a( dis(y)**2b)**-1 ~= 1 si y < min dist ou e**-(y) - min_dist si y > min dist => on calcule cela pour obtenir les valeurs sur y pour trouver a et b avec moindre carrée
+    x = np.linspace(0,10,500) # Upper bound choisis selon distance maximum entre les y (pas plus grd que 10 normalement)
+    output = []
+    for el in x:
+        if el<=min_dist:
+            output.append(1)
+        else:
+            output.append(np.exp(-el)-min_dist)
+
+    f = lambda x,a,b: 1/(1+a*np.power(x,2*b))
+    parameter,_ = op.curve_fit(f,x,output)
+
+    # m = lambda x:1/(1+parameter[0]*np.power(x,2*parameter[1])) to plot the curve
+    # l = [m(el) for el in x]
+    # plt.plot(x,l)
+    # plt.show()
+
+    return parameter[0],parameter[1] #a,b
+
+
+def main(data,min_dist,embed_dim,target_k,normalization = False,epochs = 100,learning_rate = 0.01):
+    a,b = find_best_a_b(min_dist)
+    N = data.shape[0]
+    dist = np.square(euclidean_distances(data, data))  # Matrice de distance au carré avec norme 2 -> euclidian
     rhos = [sorted(dist[i])[1] for i in range(N)]  # Distance avec le voisin le plus proche de chaque point p_i
-    proba_matrix = computeProbMatrix(sigmas,dist,rhos)
+    sigmas = np.asarray([find_optimal_sigma(target_k,rhos[i],dist[i],normalization) for i in range(len(dist))])
+    proba_matrix = p_conditional_to_joint(computeProbMatrix(sigmas,dist,rhos))
     y = np.random.rand(N,embed_dim)
-    for i in range(epochs):
-        y = y - computeGrad(proba_matrix,y)
+    for i in range(epochs): #Not SGD, standard gradient descend
+        y = y - learning_rate*computeGrad(proba_matrix,y,a,b)
     return y
 
+N = 10
+M = 15
 
-print(main(X_train,min_dist,embed_dim))
+min_dist = 0.0001# Hyperparametre influant sur a et b mais ici initialise à 1 # In practice, UMAP finds a and b from non-linear least-square fitting to the piecewise function with the min_dist hyperparameter:
+
+embed_dim = 2 #hyperparam
+TARGET_K = 5 # Number of nearest neighboor <=> k = Somme des pij sur une ligne cf p.15 doc officiel => remplace perplexité chez tsne (2^log(n) = n)
+#n_epoch -> hyperparam
+#n -> hyperparam , the number of neighbors to consider when approximating the local metric; utilise dans spectral embedding
+
+data = np.random.rand(N,M)
+
+## NORMALEMENT Y INITIALISE AVEC SPECTRAL EMBEDING A VOIR PLUS EN DETAIL, pls facon different d'initialiser y TODO
+Y = np.random.rand(N,embed_dim)
+
+print(main(data,min_dist,embed_dim,TARGET_K))
